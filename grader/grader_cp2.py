931f083527fef40f7d97c06c92b6294a24dc9e21
Matt Mukerjee <mukerjee@cs.cmu.edu>
2013-11-19 17:15:30 -0500

    second pass on dns grading scripts

--- grader/grader_cp2.py
+++ grader/grader_cp2.py
@@ -6,12 +6,18 @@ from grader_super import Project3Test, emit_scores
 
 TEST_VALUES = {
     'test_dns_simple': 2,
-    'test_dns_rr': 2
+    'test_dns_rr': 2,
+    'test_dns_lsa_topo1': 2,
+    'test_dns_lsa_topo2': 2,
+    'test_dns_lsa_topo3': 2
 }
 
 TEST_CATEGORIES = {
     'test_dns_simple': 'dns',
-    'test_dns_rr': 'dns'
+    'test_dns_rr': 'dns',
+    'test_dns_lsa_topo1': 'dns',
+    'test_dns_lsa_topo2': 'dns',
+    'test_dns_lsa_topo3': 'dns'
 }
 
 class Project3Checkpoint2Test(Project3Test):
@@ -40,7 +46,10 @@ class Project3Checkpoint2Test(Project3Test):
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     #suite.addTest(Project3Checkpoint2Test('test_dns_simple', './topos/simple-dns'))
-    suite.addTest(Project3Checkpoint2Test('test_dns_rr', './topos/rr-dns'))
+    #suite.addTest(Project3Checkpoint2Test('test_dns_rr', './topos/rr-dns'))
+    #suite.addTest(Project3Checkpoint2Test('test_dns_lsa_topo1', './topos/topo1'))
+    suite.addTest(Project3Checkpoint2Test('test_dns_lsa_topo2', './topos/topo2'))
+    #suite.addTest(Project3Checkpoint2Test('test_dns_lsa_topo3', './topos/topo3'))
     results = unittest.TextTestRunner(verbosity=2).run(suite)
 
     emit_scores(results, TEST_VALUES, TEST_CATEGORIES)
